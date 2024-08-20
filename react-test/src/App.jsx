0847d0fb02d04f6aae6a943300121a8859a783bb
orion <orion2003orion@gmail.com>
2024-08-20 16:06:19 +0300

    Added button that shows the name

--- react-test/src/App.jsx
+++ react-test/src/App.jsx
@@ -3,8 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function SomeButton({name}) {
+  const [showName, setShowName] = useState(false);
+
+  const handleClick = () => {
+    setShowName(!showName)
+  }
+
+  return (
+    <>
+      <button
+        onClick={handleClick}
+        className='name-btn'
+      >Show the name</button>
+      {showName && <div className='my-name'>{name}</div>}
+    </>
+  )
+}
+
 function App() {
-  
+  return <SomeButton name="Dmytro"></SomeButton>
 }
 
 export default App
